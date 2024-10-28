d4702a11f6b07d5a7b14cdb050e9926b529753f5
Salihou18 <skingue0906@gmail.com>
2024-10-28 10:28:00 -0400

    Ajoutez des fichiers projet.

---
+++ ApplicationBureauUA2/GestionCours.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ApplicationBureauUA2
+{
+    /// <summary>
+    /// Logique d'interaction pour GestionCours.xaml
+    /// </summary>
+    public partial class GestionCours : Window
+    {
+        public GestionCours()
+        {
+            InitializeComponent();
+        }
+    }
+}
